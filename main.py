75b97f7bea7b2a1b14414cf4cfd6bdf18302a242
InnCent <innokentiykim90@gmail.com>
2024-09-28 16:26:00 +0500

    coroutine func added

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import time
 import curses
+import asyncio
 
 
-def draw_star(canvas, star='*'):
+async def blink(canvas, row, column, symbol='*'):
     while True:
-        canvas.addstr(5, 10, star, curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(5, 10, star)
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(5, 10, star, curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(5, 10, star)
-        canvas.refresh()
-        time.sleep(0.3)
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+# def draw_star(canvas, star='*'):
+#     while True:
+#         canvas.addstr(5, 10, star, curses.A_DIM)
+#         canvas.refresh()
+#         time.sleep(2)
+#         canvas.addstr(5, 10, star)
+#         canvas.refresh()
+#         time.sleep(0.3)
+#         canvas.addstr(5, 10, star, curses.A_BOLD)
+#         canvas.refresh()
+#         time.sleep(0.5)
+#         canvas.addstr(5, 10, star)
+#         canvas.refresh()
+#         time.sleep(0.3)
 
 def draw(canvas):
     canvas.border()
-    draw_star(canvas)
-    time.sleep(1.0)    
+    coroutines = [blink(canvas, 2, i+2) for i in range(5)]
+    while True:
+        try:
+            for coro in coroutines:
+                coro.send(None)
+            canvas.refresh() 
+            time.sleep(1)
+        except StopIteration:
+            break
     
     
 if __name__ == "__main__":
